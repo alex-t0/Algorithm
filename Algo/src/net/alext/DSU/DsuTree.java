453df42c4fb12b1c7fce672335ce2cdc288873b9
Alex T <qwerty@alex-t.net>
2015-02-08 01:21:06 +0300

    Dsu tree algorithm update

--- Algo/src/net/alext/DSU/DsuTree.java
+++ Algo/src/net/alext/DSU/DsuTree.java
@@ -3,22 +3,52 @@ package net.alext.DSU;
 import java.util.ArrayList;
 
 public class DsuTree<TData> {
+    // private Dictionary<TData, TData> parents;
+
+    private int height;
+
     public class DsuNode {
-        private ArrayList<DsuNode> parents;
+        private DsuNode parent;
         private TData data;
+        // private int rank;
 
         public DsuNode(TData val) {
-            parents = new ArrayList<DsuNode>();
             data = val;
+            // rank = 0;
         }
 
         public TData getData() { return data; }
 
         public void setParent(DsuNode nodeParent){
-            parents.add(nodeParent);
+            parent = nodeParent;
         }
     }
 
-    public DsuTree() {
+    private DsuNode root;
+
+    public DsuTree(TData data) {
+        root = new DsuNode(data);
+        root.setParent(root);
+        height = 0;
+    }
+
+    public boolean Initialized(){
+        return root != null;
+    }
+
+    public void AddNode(TData value, DsuNode parent) {
+        DsuNode newNode = new DsuNode(value);
+
+        if (parent != null)
+            newNode.parent = parent;
+        else
+            newNode.parent = root;
+    }
+
+    public void Merge(DsuTree<TData> anotherTree){
+        if (anotherTree.height + 1 > height)
+            height = anotherTree.height + 1;
+
+        anotherTree.root.parent = root;
     }
 }
